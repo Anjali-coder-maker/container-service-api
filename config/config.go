c4a4f79ad29a3570a53ff1f3ca2699555a3f1b9d
nikhil <nikhil.v@qikfox.com>
2024-07-31 12:03:49 +0530

    feat(config): Add support for loading registry_service.json file
    - Updated config.go to handle loading the registry_service.json file
    - registry_service.json contains a list of all services to be added
    - Included configurations for each service in registry_service.json

--- config/config.go
+++ config/config.go
@@ -8,11 +8,12 @@ import (
 )
 
 var (
-	//go:embed default-config.json
-	defaultConfig embed.FS
+	//go:embed default-config.json registry-services.json
+	configFiles embed.FS
 
 	// config stores the configuration data
-	configData Config
+	configData   Config
+	registryData RegistryTemplates
 
 	// once ensures the configuration is loaded only once
 	once sync.Once
@@ -31,28 +32,52 @@ type Config struct {
 	Services map[string]ServiceConfig `json:"services"`
 }
 
+// ServiceTemplate represents the template configuration for a registry service
+type ServiceTemplate struct {
+	ExecStart    string `json:"exec_start"`
+	ExecStop     string `json:"exec_stop"`
+	ExecStopPost string `json:"exec_stop_post"`
+}
+
+// RegistryTemplates represents the registry services structure
+type RegistryTemplates struct {
+	Services map[string]ServiceTemplate `json:"services"`
+}
+
 // init is called automatically to load the configuration file
 func init() {
 	loadConfiguration()
 }
 
-// loadConfiguration reads the embedded configuration file and stores the values in configData
+// loadConfiguration reads the embedded configuration files and stores the values in configData and registryData
 func loadConfiguration() {
 	once.Do(func() {
-		// log.Println("Loading configuration from embedded default-config.json")
-		defaultConfigFile, err := defaultConfig.ReadFile("default-config.json")
+		// Load default configuration
+		defaultConfigFile, err := configFiles.ReadFile("default-config.json")
 		if err != nil {
 			log.Fatalf("Error reading default configuration file: %v", err)
 		}
-
 		if err := json.Unmarshal(defaultConfigFile, &configData); err != nil {
 			log.Fatalf("Error unmarshalling default configuration file: %v", err)
 		}
 
+		// Load registry services
+		registryConfigFile, err := configFiles.ReadFile("registry-services.json")
+		if err != nil {
+			log.Fatalf("Error reading registry services configuration file: %v", err)
+		}
+		if err := json.Unmarshal(registryConfigFile, &registryData); err != nil {
+			log.Fatalf("Error unmarshalling registry services configuration file: %v", err)
+		}
 	})
 }
 
 // GetConfig returns the loaded configuration
 func GetConfig() Config {
 	return configData
+}
+
+// GetRegistryTemplates returns the loaded registry services templates
+func GetRegistryTemplates() RegistryTemplates {
+	return registryData
 }
